41c3a9734c49f63b651921b077a9a0721ea367b3
Vandanova Mariya <handarkin@gmail.com>
2023-04-20 17:31:25 +0800

    после 2 ревью, не все исправлено-ошибки деплоя-2

--- backend/foodgram_api/foodgram_api/urls.py
+++ backend/foodgram_api/foodgram_api/urls.py
@@ -14,3 +14,7 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
